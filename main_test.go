f97b8ce5a38dd367b4bf92a41735e478e457be31
Kalpitha Srinivas <kalpithaasrinivas@gmail.com>
2026-03-01 05:18:58 +0000

    Trigger CI/CD pipeline

--- main_test.go
+++ main_test.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -31,37 +30,3 @@ func TestHomeHandler(t *testing.T) {
 			contentType, expected)
 	}
 }
-=======
-package main
-
-import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-)
-
-func TestHomeHandler(t *testing.T) {
-
-	req, err := http.NewRequest("GET", "/home", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-
-	handler := http.HandlerFunc(homeHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	expected := "text/html; charset=utf-8"
-	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
-		t.Errorf("wrong content type: got %v want %v",
-			contentType, expected)
-	}
-}
->>>>>>> b74ab785d831bd21ba58414f68a3dded279d536f
