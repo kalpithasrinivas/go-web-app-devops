8c4a74468d43463b1a73c9ef5d74095c39a679a8
Kalpitha Srinivas <kalpithaasrinivas@gmail.com>
2026-03-01 04:53:34 +0000

    Trigger CI Pipeline

--- main.go
+++ main.go
@@ -21,26 +21,3 @@ func main() {
 		fmt.Println("Server failed:", err)
 	}
 }
-package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/home.html")
-}
-
-func main() {
-
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/home", homeHandler)
-
-	fmt.Println("Kalpitha Go Web App running at http://localhost:8080")
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Server failed:", err)
-	}
-}
