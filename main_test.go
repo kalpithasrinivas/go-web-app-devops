879f08ee11b48bf7a395d6d812ff7cc59b7596ad
Kalpitha Srinivas <kalpithaasrinivas@gmail.com>
2026-03-01 10:01:58 +0530

    Added go-web-app with CI/CD pipeline

---
+++ main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHomeHandler(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/home", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(homeHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "text/html; charset=utf-8"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
+		t.Errorf("wrong content type: got %v want %v",
+			contentType, expected)
+	}
+}
